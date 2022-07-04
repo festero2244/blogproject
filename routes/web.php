46c578d7bce178f1439c7066d68676df49b1a0ea
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2022-07-04 22:54:06 +0300

    Rename index route to /

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/index',[PagesController::class,'index']);
+Route::get('/',[PagesController::class,'index']);
 
 Route::resource('/blog',PostsController::class);
 
